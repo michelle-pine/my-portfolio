226019c3caf51b47d1eb13e45c8507907816a514
Michelle <michelle.pine33@gmail.com>
2018-05-11 19:58:03 -0400

    p5 fix, added is library

--- src/assets/javascript/fearless-flight/fearless-flight-permissions.js
+++ src/assets/javascript/fearless-flight/fearless-flight-permissions.js
@@ -1,26 +1,40 @@
 var permissionFF = false;
 
 $(document).ready(function() {
-    if (window.location.pathname == "/projects/fearless_flight") {
-        permissionFF = true;
-        $.getScript("/assets/javascript/libraries/p5.sound.js").done(function() {
-            $.getScript("/assets/javascript/fearless-flight/fearless-flight.js", function() {
-            });
-        });
+  if (window.location.pathname == "/projects/fearless_flight") {
+    if (is.desktop()) {
+      var head = document.getElementsByTagName("head")[0];
+      var script = document.createElement("script");
+      script.type = "text/javascript";
+      script.onload = function() {
+        callFunctionFromScript();
+      };
+      script.src = "/assets/javascript/libraries/p5.sound.js";
+      head.appendChild(script);
+      permissionFF = true;
+      var head = document.getElementsByTagName("head")[0];
+      var script = document.createElement("script");
+      script.type = "text/javascript";
+      script.onload = function() {
+        callFunctionFromScript();
+      };
+      script.src = "/assets/javascript/fearless-flight/fearless-flight.js";
+      head.appendChild(script);
     }
-})
-
-$(document).on('click', '#fearless_flight', function () {
-    location.reload();
-    
+    if (is.not.desktop()) {
+      $("#flight-contain").html(
+        "This project is not supported on mobile or tablet."
+      );
+    }
+  }
 });
-$(document).on('click', 'body', function () {
-    if (window.location.href.indexOf("/projects/fearless_flight") > -1) {
 
-        
-    }
-    else {
-        permissionFF = false;
-
-    }
+$(document).on("click", "#fearless_flight", function() {
+  location.reload();
+});
+$(document).on("click", "body", function() {
+  if (window.location.href.indexOf("/projects/fearless_flight") > -1) {
+  } else {
+    permissionFF = false;
+  }
 });
--- src/assets/javascript/windy-weather/windy-weather-permissions.js
+++ src/assets/javascript/windy-weather/windy-weather-permissions.js
@@ -1,25 +1,28 @@
 var permissionWW = false;
 
 $(document).ready(function() {
-    if (window.location.pathname == "/projects/windy_weather") {
-        permissionWW = true;
-        $.getScript("/assets/javascript/windy-weather/windy-weather.js", function() {
-        });
-
-       
+  if (window.location.pathname == "/projects/windy_weather") {
+    if (is.desktop()) {
+      permissionWW = true;
+      $.getScript(
+        "/assets/javascript/windy-weather/windy-weather.js",
+        function() {}
+      );
     }
-
-})
-
-$(document).on('click', '#windy_weather', function () {
-    location.reload();
-
+    if (is.not.desktop()) {
+      $("#flight-contain").html(
+        "This project is not supported on mobile or tablet."
+      );
+    }
+  }
 });
-$(document).on('click', 'body', function () {
-    if (window.location.href.indexOf("/projects/windy_weather") > -1) {
-    }
-    else {
-        permissionWW = false;
 
-    }
+$(document).on("click", "#windy_weather", function() {
+  location.reload();
+});
+$(document).on("click", "body", function() {
+  if (window.location.href.indexOf("/projects/windy_weather") > -1) {
+  } else {
+    permissionWW = false;
+  }
 });
