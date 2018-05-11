93e54c49c31b4f598203aca4362c0d3e6cfaec84
Pine <CH200004@CARDIO.CHBOSTON.ORG>
2018-05-11 08:45:17 -0400

    Auto-generated commit

--- assets/javascript/fearless-flight/fearless-flight-permissions.js
+++ assets/javascript/fearless-flight/fearless-flight-permissions.js
@@ -2,7 +2,12 @@ var permissionFF = false;
 
 $(document).ready(function() {
     if (window.location.pathname == "/projects/fearless_flight") {
-
+        $.getScript("/assets/javascript/libraries/p5.js", function() {
+        });
+        $.getScript("/assets/javascript/libraries/p5.dom.js", function() {
+        });
+        $.getScript("/assets/javascript/libraries/p5.sound.js", function() {
+        });
         $.getScript("/assets/javascript/fearless-flight/fearless-flight.js", function() {
         });
         permissionFF = true;
--- assets/javascript/windy-weather/windy-weather-permissions.js
+++ assets/javascript/windy-weather/windy-weather-permissions.js
@@ -3,6 +3,12 @@ var permissionWW = false;
 $(document).ready(function() {
     if (window.location.pathname == "/projects/windy_weather") {
         permissionWW = true;
+        $.getScript("/assets/javascript/libraries/p5.js", function() {
+        });
+        $.getScript("/assets/javascript/libraries/p5.dom.js", function() {
+        });
+        $.getScript("/assets/javascript/libraries/p5.sound.js", function() {
+        });
         $.getScript("/assets/javascript/windy-weather/windy-weather.js", function() {
         });
 
