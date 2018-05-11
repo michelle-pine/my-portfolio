e436765fba84e171e8eb720b65134ed48078b696
Pine <CH200004@CARDIO.CHBOSTON.ORG>
2018-05-11 09:31:18 -0400

    path fix

--- src/assets/javascript/fearless-flight/fearless-flight-permissions.js
+++ src/assets/javascript/fearless-flight/fearless-flight-permissions.js
@@ -3,8 +3,8 @@ var permissionFF = false;
 $(document).ready(function() {
     if (window.location.pathname == "/projects/fearless_flight") {
         permissionFF = true;
-        $.getScript("./assets/javascript/libraries/p5.sound.js").done(function() {
-            $.getScript("./assets/javascript/fearless-flight/fearless-flight.js", function() {
+        $.getScript("/assets/javascript/libraries/p5.sound.js").done(function() {
+            $.getScript("/assets/javascript/fearless-flight/fearless-flight.js", function() {
             });
         });
     }
