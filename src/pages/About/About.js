09b072c90a306eb2e2ea0f354a3d96ea9bf90a00
michelle-pine <michelle.pine33@gmail.com>
2023-02-05 14:56:42 -0500

    updates

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,61 +1,190 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './About.scss';
-import Title from '../../components/Title';
-import Skills from '../../components/Skills';
-import Experience from '../../components/Experience';
+import React from "react";
+import PropTypes from "prop-types";
+import "./About.scss";
+import Title from "../../components/Title";
+import Skills from "../../components/Skills";
+import Experience from "../../components/Experience";
 
-import { Link } from 'react-router-dom';
-
+import { Link } from "react-router-dom";
 
 class About extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
   }
 
   render() {
     return (
-    <div className="page about-page">
-      <Title title="About" />
-      <div className="about-bio">
-        <div className="about-bio-image"/>
-        <div className="about-bio-info">
-          <strong>My name is Michelle Pine. I'm a Boston-based developer, designer, video editor, aspiring writer, and incurable dog-lover!</strong>
-          <p>As a computer scientist and designer, I'm constantly exploring ways to use code and visuals to display my ideas in creative and exciting ways. I generally focus on front-end development and UI/UX design, but I have back-end development experience as well. Check out my <Link to="/">projects</Link> for a taste of what I've done so far.</p>
-          <p>Right now, I'm a senior at <strong>Northeastern University</strong> in Boston, where I'm pursuing a combined major in my passions: Computer Science and Design. Currently, I'm the president of <strong>Northeastern University Women in Technology (nuWIT)</strong>, a gender-diversity focused student organization. I also spend much of my free time writing and hope to be a novelist one day.</p>
+      <div className="page about-page">
+        <Title title="About" />
+        <div className="about-bio">
+          <div className="about-bio-image" />
+          <div className="about-bio-info">
+            <strong>
+              My name is Michelle Pine. I'm a Boston-based software engineer,
+              designer, video editor, aspiring writer, and incurable dog-lover!
+            </strong>
+            <p>
+              As a computer scientist and designer, I'm constantly exploring
+              ways to use code and visuals to display my ideas in creative and
+              exciting ways. I generally focus on front-end development and
+              UI/UX design, but I have back-end development experience as well.
+              Check out my <Link to="/">projects</Link> for a taste of what I've
+              done so far.
+            </p>
+            <p>
+              Right now, I'm a Frontend Technical Lead at HubSpot, where we're
+              always trying to discover new technical solutions for our
+              customers. In my free time, I love to write and hope to be a
+              novelist one day.
+            </p>
+          </div>
+        </div>
+        <Title title="Experience" overrideTitle="About" />
+        <div className="experience-set">
+          <Experience
+            organization="HubSpot"
+            position="Frontend Technical Lead"
+            year="Nov 2022 – Present"
+          ></Experience>
+          <Experience
+            organization="HubSpot"
+            position="Senior Software Engineer"
+            year="Dec 2021 – Nov 2022"
+            description="Produced critical, comprehensive documentation about new & legacy systems. Improved app performance/memory by facilitating proper Javascript treeshaking, reducing load time by several hundred milliseconds & file sizes by 50%. Advocated and organized cross-library refactors which achieved near-complete screen reader accessibility in team-owned applications."
+          ></Experience>
+          <Experience
+            organization="HubSpot"
+            position="Software Engineer"
+            year="Jun 2021 – Dec 2021"
+            description="Supervised application/library builds, deploys, and rollbacks. Assumed engineer on-call duties which included monitoring, triage, & debugging. Resolved technical debt and rearchitected significant swathes of legacy code. Mentored & supported peers, interns, & new hires."
+          ></Experience>
+          <Experience
+            organization="HubSpot"
+            position="Software Engineering Co-op"
+            year="Jan 2020 – Jun 2021"
+            description="Implemented redesign of email interface using custom React components. Refactored libraries and apps to facilitate introduction of abstract data types. Instituted comprehensive enzyme/jasmine & selenium test suites."
+          ></Experience>
+          <Experience
+            organization="BookBub"
+            position="Software Engineering Co-op"
+            description="Programmed component library adhering to design and accessibility standards. Extended full-stack web apps using Ruby, Rails, PostgreSQL, & React. Developed custom mass marketing emails using a Clojure templater system &  internal APIs."
+            year="Jan 2019 - Dec 2019"
+          ></Experience>
+          <Experience
+            organization="Boston Children's Hospital"
+            description="Developed and maintained front-end interface of four different web applications using Angular, Bootstrap, and internal APIs. Designed modern and highly usable interfaces for new & existing hospital apps. Adhered to principles of collaborative scrum/agile development."
+            position="Web UI Innovator and Graphics Designer"
+            year="Jan 2018 - June 2018"
+          ></Experience>
+          <Experience
+            organization="Harold Grinspoon Foundation"
+            description="Designed various social media graphics; Composed, edited, and animated book trailers and other promotional videos; Marketed the organization through social media copy."
+            position="Social Media Intern and Freelancer"
+            year="July 2016 - June 2018"
+          ></Experience>
+        </div>
+        <Title title="Skills" overrideTitle="About" />
+        <div className="skill-set">
+          <Skills
+            header="Languages"
+            icon="fa-code"
+            skills={[
+              "Ruby",
+              "Java",
+              "Javascript",
+              "PostgreSQL",
+              "Typescript",
+              "HTML",
+              "CSS",
+              "R",
+              "Python",
+              "C#",
+              "Clojure",
+              "Racket",
+              "Command Line",
+            ]}
+          />
+          <Skills
+            header="Libraries"
+            icon="fa-file-archive-o"
+            skills={[
+              "React",
+              "Redux",
+              "GraphQL",
+              "Flux",
+              "Unity",
+              "React Flow",
+              "ElkJS",
+              "Selenium",
+              "Jasmine",
+              "React Testing Library",
+              "Enzyme",
+              "Bootstrap",
+              "Angular",
+              "SCSS",
+              "LESS",
+              "p5",
+              "Processing",
+            ]}
+          />
+          <Skills
+            header="Design"
+            icon="fa-crop"
+            skills={[
+              "Photoshop",
+              "Illustrator",
+              "Premiere Pro",
+              "InDesign",
+              "After Effects",
+              "Figma",
+              "Marvel",
+              "Invision",
+              "Balsamiq",
+            ]}
+          />
+          <Skills
+            header="More"
+            icon="fa-plus-circle"
+            skills={[
+              "Project Management",
+              "Interaction Design",
+              "Graphic Design",
+              "User Testing",
+              "Video Editing",
+              "Photo Editing",
+              "Public Speaking",
+              "Social Media",
+              "Copywriting",
+              "Editing",
+              "Accessibility Auditing",
+            ]}
+          />
+        </div>
+        <Title title="Leadership" overrideTitle="About" />
+        <div className="leadership-set">
+          <Experience
+            organization="Northeastern University Women in Technology"
+            position="President"
+            year="2019-2020"
+          ></Experience>
+          <Experience
+            organization="Khoury College Student Advisory Board"
+            position="Member"
+            year="2019-present"
+          ></Experience>
+          <Experience
+            organization="Northeastern University Women in Technology"
+            position="Web Chair"
+            year="2018-2019"
+          ></Experience>
         </div>
       </div>
-      <Title title="Leadership" overrideTitle="About" />
-      <div className="leadership-set">
-        <Experience organization="Northeastern University Women in Technology" position="President" year="2019-present"></Experience>
-        <Experience organization="Khoury College Student Advisory Board" position="Member" year="2019-present"></Experience>
-        <Experience organization="Northeastern University Women in Technology" position="Web Chair" year="2018-2019"></Experience>
-      </div>
-      <Title title="Experience" overrideTitle="About"/>
-      <div className="experience-set">
-        <Experience organization="HubSpot" position="Software Engineering Co-op" year="present"></Experience>
-        <Experience organization="BookBub" position="Software Engineering Co-op"  description="Edited, maintained, and developed the BookBub software using React, Ruby on Rails, Postgres, automated tests, and continuous integration practices." year="Jan 2019 - Dec 2019"></Experience>
-        <Experience organization="Boston Children's Hospital" description="Designed, implemented, and improved the front-end design of various Heart Center web applications, utilizing technologies such as Angular, Javascript, HTML, CSS, Balsamiq, SCSS, and LESS" position="Web UI Innovator and Graphics Designer" year="Jan 2018 - June 2018"></Experience>
-        <Experience organization="Harold Grinspoon Foundation" description="Designed various social media graphics; Composed, edited, and animated book trailers and other promotional videos; Marketed the organization through social media copy." position="Social Media Intern and Freelancer" year="July 2016 - June 2018"></Experience>
-      </div>
-      <Title title="Skills" overrideTitle="About"/>
-      <div className="skill-set">
-        <Skills header="Languages" icon="fa-code" skills={["Ruby","Java","Javascript","PostgreSQL","R","Python","Typescript","HTML","CSS","SCSS","LESS","Racket","Command Line"]}/>
-        <Skills header="Libraries" icon="fa-file-archive-o" skills={["Bootstrap","Angular","React","p5","Processing"]}/>
-        <Skills header="Design" icon="fa-crop" skills={["Photoshop","Illustrator","Premiere Pro","InDesign","After Effects","Figma", "Marvel","Invision","Balsamiq"]}/>
-        <Skills header="More" icon="fa-plus-circle" skills={["Public Speaking","Social Media","Copywriting"]}/>
-      </div>
-    </div>
     );
   }
-};
+}
 
-About.defaultProps = {
+About.defaultProps = {};
 
-};
-
-About.propTypes = {
-
-};
+About.propTypes = {};
 
 export default About;
--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -1,63 +1,65 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './Projects.scss';
+import React from "react";
+import PropTypes from "prop-types";
+import "./Projects.scss";
 
-import Title from '../../components/Title';
-import Project from '../../components/Project';
-import projects from '../../data/projects';
-import michelle from '../../media/michelle1.jpeg';
+import Title from "../../components/Title";
+import Project from "../../components/Project";
+import projects from "../../data/projects";
+import michelle from "../../media/michelle1.jpeg";
 import MultiSelect from "@khanacademy/react-multi-select";
 
 const yearOptions = [
-  {label: "2020", value: 2020},
-  {label: "2019", value: 2019},
-  {label: "2018", value: 2018},
-  {label: "2017", value: 2017},
-  {label: "2016", value: 2016},
-]
+  { label: "2020", value: 2020 },
+  { label: "2019", value: 2019 },
+  { label: "2018", value: 2018 },
+  { label: "2017", value: 2017 },
+  { label: "2016", value: 2016 },
+];
 
 const typeOptions = [
-  {label: "design", value: "design"},
-  {label: "coding", value: "coding"},
-  {label: "video", value: "video"},
-  {label: "writing", value: "writing"},
-]
+  { label: "design", value: "design" },
+  { label: "coding", value: "coding" },
+  { label: "video", value: "video" },
+  { label: "writing", value: "writing" },
+];
 
 class Projects extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       yearSelected: [],
       typeSelected: [],
       searchValue: "",
       displayedProjects: projects,
-    }
+    };
     this.onTypeSelect = this.onTypeSelect.bind(this);
     this.onYearSelect = this.onYearSelect.bind(this);
     this.onSearch = this.onSearch.bind(this);
   }
 
   onYearSelect(selected) {
-    this.setState({yearSelected: selected});
+    this.setState({ yearSelected: selected });
   }
 
   onTypeSelect(selected) {
-    this.setState({typeSelected: selected});
+    this.setState({ typeSelected: selected });
   }
 
   onSearch(e) {
     const search = e.target.value;
-    this.setState({searchValue: search});
+    this.setState({ searchValue: search });
   }
 
   hasAnyMatch(typeSelected, proj) {
     let match = false;
-    proj.type.map(function(value) { match = match || typeSelected.indexOf(value) !== -1})
+    proj.type.map(function (value) {
+      match = match || typeSelected.indexOf(value) !== -1;
+    });
     return match;
   }
 
   matchesSearch(project, searchValue) {
-    let specialChars = "!@#$^&%*()+=-[]\/{}|:<>?,.";
+    let specialChars = "!@#$^&%*()+=-[]/{}|:<>?,.";
 
     let nameUpper = project.title.toUpperCase();
     let descriptionUpper = project.description.toUpperCase();
@@ -65,91 +67,106 @@ class Projects extends React.Component {
     for (var i = 0; i < specialChars.length; i++) {
       string2 = string2.replace(new RegExp("\\" + specialChars[i], "gi"), "");
     }
-    let regex = new RegExp( string2, 'g' );
+    let regex = new RegExp(string2, "g");
     let matchesName = nameUpper.match(regex);
     let matchesDescription = descriptionUpper.match(regex);
     return matchesName || matchesDescription;
   }
 
   filterProjects(yearSelected, typeSelected, searchValue) {
-    return projects.filter(function(proj) {
-      return (yearSelected.length === 0 || yearSelected.indexOf(proj.year) !== -1)
-        && (typeSelected.length === 0 || this.hasAnyMatch(typeSelected, proj))
-        && (!searchValue || searchValue.length === 0 || this.matchesSearch(proj, searchValue));
-    }.bind(this));
+    return projects.filter(
+      function (proj) {
+        return (
+          (yearSelected.length === 0 ||
+            yearSelected.indexOf(proj.year) !== -1) &&
+          (typeSelected.length === 0 || this.hasAnyMatch(typeSelected, proj)) &&
+          (!searchValue ||
+            searchValue.length === 0 ||
+            this.matchesSearch(proj, searchValue))
+        );
+      }.bind(this)
+    );
   }
 
   renderProjects(filteredProjects) {
-    
-    return filteredProjects.map(function(proj) {
-      return <Project 
-        project={proj}
-        single={filteredProjects.length === 1}
-      />
+    return filteredProjects.map(function (proj) {
+      return <Project project={proj} single={filteredProjects.length === 1} />;
     });
   }
 
   render() {
-    const {yearSelected, typeSelected, searchValue} = this.state;
-    const filteredProjects = this.filterProjects(yearSelected, typeSelected, searchValue);
+    const { yearSelected, typeSelected, searchValue } = this.state;
+    const filteredProjects = this.filterProjects(
+      yearSelected,
+      typeSelected,
+      searchValue
+    );
     return (
-    <div className="projects-page page">
-      <Title title="Projects"/>
-      <div className="info-section">
-        <div style={{backgroundImage: `url(${michelle})`}} className="info-photo"/>
-        <div className="info-description">
-          Michelle Pine is a student, designer, developer, and writer specializing in client-facing technology. 
-        </div>
-      </div>
-      <div className="projects-filter">
-        <form className="searchbox">
-          <label htmlFor="search-box" className="sr-only">Search</label>
-          <input id="search-box" onChange={this.onSearch} name="searchbox" type="text" placeholder="Search..."/>
-          <i className="fa fa-search search-icon"></i>
-        </form>
-        <div className="year-filter">
-          <MultiSelect
-            options={yearOptions}
-            selected={yearSelected}
-            disableSearch
-            onSelectedChanged={this.onYearSelect}
-            overrideStrings={{
-              selectSomeItems: "Year...",
-              allItemsAreSelected: "All Years",
-              selectAll: "Select All",
-              search: "Search",
-            }}
+      <div className="projects-page page">
+        <Title title="Projects" />
+        <div className="info-section">
+          <div
+            style={{ backgroundImage: `url(${michelle})` }}
+            className="info-photo"
           />
+          <div className="info-description">
+            Michelle Pine is an experienced software engineer, designer, and
+            aspiring writer specializing in client-facing technology.
+          </div>
         </div>
-        <div className="type-filter">
-          <MultiSelect
-            options={typeOptions}
-            disableSearch
-            selected={typeSelected}
-            onSelectedChanged={this.onTypeSelect}
-            overrideStrings={{
-              selectSomeItems: "Project Type...",
-              allItemsAreSelected: "All Project Types",
-              selectAll: "Select All",
-              search: "Search",
-            }}
-          />
+        <div className="projects-filter">
+          <form className="searchbox">
+            <label htmlFor="search-box" className="sr-only">
+              Search
+            </label>
+            <input
+              id="search-box"
+              onChange={this.onSearch}
+              name="searchbox"
+              type="text"
+              placeholder="Search..."
+            />
+            <i className="fa fa-search search-icon"></i>
+          </form>
+          <div className="year-filter">
+            <MultiSelect
+              options={yearOptions}
+              selected={yearSelected}
+              disableSearch
+              onSelectedChanged={this.onYearSelect}
+              overrideStrings={{
+                selectSomeItems: "Year...",
+                allItemsAreSelected: "All Years",
+                selectAll: "Select All",
+                search: "Search",
+              }}
+            />
+          </div>
+          <div className="type-filter">
+            <MultiSelect
+              options={typeOptions}
+              disableSearch
+              selected={typeSelected}
+              onSelectedChanged={this.onTypeSelect}
+              overrideStrings={{
+                selectSomeItems: "Project Type...",
+                allItemsAreSelected: "All Project Types",
+                selectAll: "Select All",
+                search: "Search",
+              }}
+            />
+          </div>
+        </div>
+        <div className="projects-container">
+          {this.renderProjects(filteredProjects)}
         </div>
       </div>
-      <div className="projects-container">
-        {this.renderProjects(filteredProjects)}
-      </div>
-    </div>
     );
   }
-};
+}
 
-Projects.defaultProps = {
+Projects.defaultProps = {};
 
-};
-
-Projects.propTypes = {
-
-};
+Projects.propTypes = {};
 
 export default Projects;
